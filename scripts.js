543d82b643d0f944468e9f12f0ec1f41b6623358
Tomas <hlavkat@gmail.com>
2023-07-31 16:27:26 +0200

    remove progress

--- scripts.js
+++ scripts.js
@@ -1,13 +1,10 @@
-function showProgressHtml(id) {
-  document.querySelector(id).classList.toggle("d-always");
-}
 function markUpProgress(dataType) {
   document
     .querySelectorAll(`*[data-type='${dataType}']`)
-    .forEach((e) => e.classList.add("mark-up-progress"));
+    .forEach((e) => e.classList.add("mark-up-icon"));
 }
 function removeMarkUpProgress(dataType) {
   document
     .querySelectorAll(`*[data-type='${dataType}']`)
-    .forEach((e) => e.classList.remove("mark-up-progress"));
+    .forEach((e) => e.classList.remove("mark-up-icon"));
 }
